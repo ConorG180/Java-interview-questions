4665c1194d04dd5baca9775f2aaf50fc3b3ce7e2
Conor <conor.ganly@yahoo.com>
2023-03-12 16:47:56 +0000

    Add solution for  Fizzbuzz interview problem

--- src/FizzBuzz/Main.java
+++ src/FizzBuzz/Main.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+//        Fizzbuzz problem
+        System.out.print("Hello! Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if(number % 15 == 0)
+            System.out.println("FizzBuzz!");
+        else if(number % 5 == 0)
+            System.out.println("Fizz!");
+        else if(number % 3 == 0)
+            System.out.println("Buzz!");
+        else
+            System.out.println(number);
     }
 }
